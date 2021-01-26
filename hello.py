d03ef94cec36ddef2475e66848a95b2da18e9746
Guillem Amat <guillem.amat@duke.edu>
2021-01-26 23:19:55 +0000

    adding initial structure

---
+++ hello.py
@@ -0,0 +1,11 @@
+"""This is this .py file's docstring"""
+
+
+def add(first, second):
+    """This is this function's docstring"""
+    return first + second
+
+
+RESULT = add(1, 2)
+
+print(f"This is the sum: 1, 2, {RESULT}")
